754f6569469b0a8c2a2b061655690628ccbeddff
Alexandre Andrieux <alexandre.andrieux@simplx.fr>
2018-03-22 16:19:37 +0100

    Fix CORS issue by allowing everyone, huehue #R0003

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('.'));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => res.send('HEllo express'));
 app.get('/hey', (req, res) => res.send('HEllo hey'));
 
@@ -12,5 +19,3 @@ app.post('/pay', (req, res) => {
 
 app.listen(3000, () => console.log('Example app listening on port 3K'));
 app.listen(3001, () => console.log('Example app listening on port 3001'));
-
-app.use(express.static('.'));
