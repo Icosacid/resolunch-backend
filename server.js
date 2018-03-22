72d49f692b90fa94622ea8dc38bb846587ae1372
Alexandre Andrieux <alexandre.andrieux@simplx.fr>
2018-03-22 17:41:37 +0100

    Add body-parser and have POSTs work #R0003

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('.'));
@@ -7,14 +8,24 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(bodyParser.urlencoded({// Makes POSTs work
+    extended: true,
+}));
+app.use(bodyParser.json({limit: '2mb'}));
 
 app.get('/', (req, res) => res.send('HEllo express'));
 app.get('/hey', (req, res) => res.send('HEllo hey'));
 
-app.post('/pay', (req, res) => {
-	console.log('req');
-	console.log(req);
-	res.send(100 * Math.random() + '€');
+app.post('/lunch-request', (req, res) => {
+	console.log('POST on /lunch-request with: ', req.body);
+
+    if (!req || !req.body) {
+        return res.status(400).send('Invalid data !');
+    }
+
+	//console.log('POST on /lunch-request returning: ', req);
+	return res.status(200).send(req.body);
+
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3K'));
